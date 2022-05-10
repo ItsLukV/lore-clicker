2d2a1ba27af06d4439fb85a1d911318e6a8513fd
Lukas <lukasvranic@gmail.com>
2022-05-10 20:55:06 +0200

    tilføjde filler lore

--- script.js
+++ script.js
@@ -1,13 +1,28 @@
-document.getElementById("btn").onclick = function() {click()};
+document.getElementById("btn").onclick = function () {
+  click();
+};
+let navn;
 
 var clicks = 0;
 function click() {
-    console.log("test")
-    clicks = clicks +  1;
-   document.getElementById("text").innerHTML = clicks
-   if (clicks === 10) {
-       alert("You have clicked 10 times")
-   } else if (clicks === 15) {
-       alert("You have clicked 15 times")
-   }
+  console.log("test");
+  clicks = clicks + 1;
+  document.getElementById("text").innerHTML = clicks;
+  switch (clicks) {
+    case 10:
+      navn = prompt("Hej halløj, rejsende.\nHvad er dit navn?", "Harry Potter");
+      alert("Hej " + navn + "!");
+      break;
+    case 20:
+      alert(
+        "Donec cursus gravida laoreet. Donec dictum quam vel massa bibendum, nec tempor diam vestibulum."
+      );
+      break;
+    case 30:
+      alert("Donec eget nunc eget nunc ornare aliquam.");
+      break;
+    case 40:
+      alert("Nunc ex tellus, facilisis sed nisl at, tincidunt rhoncus felis.");
+      break;
+  }
 }
