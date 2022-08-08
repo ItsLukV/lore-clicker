1a0ad0f2f9da2d0b32d7bdb5f0f595bf7d904ca0
Lukas Vranic <Lukasvranic@gmail.com>
2022-08-08 09:37:16 +0200

    switch to p5

--- script.js
+++ script.js
@@ -1,28 +1,28 @@
-document.getElementById("btn").onclick = function () {
-  click();
-};
-let navn;
+// document.getElementById("btn").onclick = function () {
+//   click();
+// };
+// let navn;
 
-var clicks = 0;
-function click() {
-  console.log("test");
-  clicks = clicks + 1;
-  document.getElementById("text").innerHTML = clicks;
-  switch (clicks) {
-    case 10:
-      navn = prompt("Hej halløj, rejsende.\nHvad er dit navn?", "Harry Potter");
-      alert("Hej " + navn + "!");
-      break;
-    case 20:
-      alert(
-        "Donec cursus gravida laoreet. Donec dictum quam vel massa bibendum, nec tempor diam vestibulum."
-      );
-      break;
-    case 30:
-      alert("Donec eget nunc eget nunc ornare aliquam.");
-      break;
-    case 40:
-      alert("Nunc ex tellus, facilisis sed nisl at, tincidunt rhoncus felis.");
-      break;
-  }
-}
+
+// var clicks = 0;
+// function click() {
+//   clicks = clicks + 1;
+//   document.getElementById("text").innerHTML = clicks;
+//   switch (clicks) {
+//     case 10:
+//       navn = prompt("Hej halløj, rejsende.\nHvad er dit navn?", "Harry Potter");
+//       alert("Hej " + navn + "!");
+//       break;
+//     case 20:
+//       alert(
+//         "Donec cursus gravida laoreet. Donec dictum quam vel massa bibendum, nec tempor diam vestibulum."
+//       );
+//       break;
+//     case 30:
+//       alert("Donec eget nunc eget nunc ornare aliquam.");
+//       break;
+//     case 40:
+//       alert("Nunc ex tellus, facilisis sed nisl at, tincidunt rhoncus felis.");
+//       break;
+//   }
+// }
