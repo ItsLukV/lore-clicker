a3647d59fb2cd235d69fb026c6ba011a78b19178
Lukas Vranic <Lukasvranic@gmail.com>
2022-08-15 08:52:40 +0200

    update

--- sketch.js
+++ sketch.js
@@ -1,90 +1,11 @@
-let clicks = 0;
-let clicksShow;
-let skive;
-let shop;
-
-
+function preload() {
+  img = loadImage("assets/clicker.png");
+}
 function setup() {
-    load();
-
-    clicksShow = createP(clicks);
-    clicksShow.parent(main);
-
-    // position stuff
-    clicksShow.style("position","absolute")
-    clicksShow.style("left",`${50}%`)
-    // clicksShow.style("top",`${50}%`)
-    clicksShow.style("transform","translate(-50%,-50%)")
-    clicksShow.html(`${playerData.clicks} clicks`)
-
-    // Shop create
-    shop = createA("shop.html","Shop")
-    shop.parent(main)
-    shop.addClass("shop")
-
-    // Shop pos
-    shop.style("position","absolute")
-    shop.style("left",`${50}%`)
-    shop.style("top",`${600}%`)
-
-    // Shop looks
-    shop.style("background-color","rgb(120,120,120)")
-    shop.style("padding","10px 10px 10px 10px")
-    shop.style("border-radius","2.5%")
-    shop.style("border", "2px solid black")
-    shop.style("transform","translate(-50%,-50%)")
-    shop.style("text-decoration","none")
-    shop.style("color","inherit")
-    
-
-
-    skive = new Skive();
-
+  load();
+  createCanvas(400, 400);
 }
 
-let frames = 0;
 function draw() {
-    if(frames == 60) {
-        playerData.clicks += playerData.shop.slave * 1
-        playerData.clicks += playerData.shop.superSlave * 2
-        frames = 0;    
-    }
-    frames++;
-
-    clicksShow.html(`${playerData.clicks} clicks`)
+  image(img, 0, 0);
 }
-
-class Skive {
-    constructor() {
-        this.width = 500;
-        this.height = 500;
-        this.skive = createDiv();
-        this.skive.parent(main)
-
-        // Looks Stuff
-        this.skive.style("width",`${this.width}px`);
-        this.skive.style("height",`${this.height}px`);
-
-        // Class Stuff for css stuff
-        this.skive.addClass("skive")
-        this.skive.addClass("hover");
-
-        // position stuff
-        this.skive.style("position","absolute")
-        this.skive.style("left",`${50}%`)
-        this.skive.style("top",`${50}%`)
-        this.skive.style("transform","translate(-50%)")
-
-
-
-        // Clicking stuff
-        this.skive.mousePressed(() => {
-            playerData.clicks += playerData.shop.mouse;
-            clicksShow.html(`${playerData.clicks} clicks`);
-        })
-    }
-}
-
-
-
-
